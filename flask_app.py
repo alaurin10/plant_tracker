6e3d109c9d06bff9009e28532822dad339a577c5
Andrew Laurin <alaurin10@gmail.com>
2023-01-26 21:02:00 -0800

    changes

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, timedelta
 from config import DevelopmentConfig
